82721a6056c5934386becaf416209b005e9c2a99
LeonardoTrapani <leonard.trapani@gmail.com>
2025-05-06 18:29:45 +0200

    improve formatting on save

--- nvim/lua/trapani/lazy/format.lua
+++ nvim/lua/trapani/lazy/format.lua
@@ -1,7 +1,18 @@
 return {
 	"sbdchd/neoformat",
-
 	config = function()
-		vim.keymap.set("n", "<leader>fo", "<cmd>Neoformat<CR>") -- neoformat formatting (uses prettier or other formatters)
+		-- Set up keymaps
+		vim.keymap.set("n", "<leader>fo", "<cmd>Neoformat<CR>", { desc = "Format with Neoformat" })
+		
+		-- Enable async formatting on save with a small delay
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function()
+				local bufnr = vim.api.nvim_get_current_buf()
+				vim.defer_fn(function()
+					vim.cmd("Neoformat")
+				end, 100) -- 100ms delay
+			end,
+		})
 	end,
 }
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -71,3 +71,8 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Created by `pipx` on 2025-02-03 14:45:25
 export PATH="$PATH:/Users/leonardotrapani/.local/bin"
+export PATH="/opt/homebrew/opt/node@20/bin:$PATH"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
