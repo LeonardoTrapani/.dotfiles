882db7c5de493cdf3b3a69e1f40b9ec9088690dc
LeonardoTrapani <leonard.trapani@gmail.com>
2025-05-08 14:09:22 +0200

    fix mason config

--- nvim/lua/trapani/lazy/lsp.lua
+++ nvim/lua/trapani/lazy/lsp.lua
@@ -3,13 +3,7 @@ return {
 	dependencies = {
 		-- LSP Support
 		{ "neovim/nvim-lspconfig" },
-		{
-			"williamboman/mason.nvim",
-			build = function()
-				pcall(vim.cmd, "MasonUpdate")
-			end,
-		},
-		{ "williamboman/mason-lspconfig.nvim" }, -- Optional
+		{ "mason-org/mason.nvim" },
 
 		-- Autocompletion
 		{ "hrsh7th/nvim-cmp" }, -- Required
@@ -40,67 +34,6 @@ return {
 
 		require("fidget").setup({})
 		require("mason").setup()
-
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"lua_ls",
-				"rust_analyzer",
-				"eslint",
-				"html",
-				"jsonls",
-				"tailwindcss",
-			},
-			handlers = {
-				function(server_name) -- default handler (optional)
-					require("lspconfig")[server_name].setup({
-						capabilities = capabilities,
-					})
-				end,
-
-				zls = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.zls.setup({
-						root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-						settings = {
-							zls = {
-								enable_inlay_hints = true,
-								enable_snippets = true,
-								warn_style = true,
-							},
-						},
-					})
-					vim.g.zig_fmt_parse_errors = 0
-					vim.g.zig_fmt_autosave = 0
-				end,
-				["lua_ls"] = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.lua_ls.setup({
-						capabilities = capabilities,
-						settings = {
-							Lua = {
-								runtime = { version = "Lua 5.1" },
-								diagnostics = {
-									globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-								},
-							},
-						},
-					})
-				end,
-				["pyright"] = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.pyright.setup({
-						capabilities = capabilities,
-						settings = {
-							python = {
-								analysis = {
-									typeCheckingMode = "off",
-								},
-							},
-						},
-					})
-				end,
-			},
-		})
 
 		local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
